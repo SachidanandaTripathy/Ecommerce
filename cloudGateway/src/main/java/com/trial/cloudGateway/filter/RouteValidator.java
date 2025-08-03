7ec2a2bacfed9ff20b013fa8f4261a04da580519
SachidanandaTripathy <Sachidananda.tripathy03@gmail.com>
2025-08-03 23:32:51 +0530

    Spring cloud gateway implemented successfully

---
+++ cloudGateway/src/main/java/com/trial/cloudGateway/filter/RouteValidator.java
@@ -0,0 +1,22 @@
+package com.trial.cloudGateway.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/login"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
---
+++ cloudGateway/src/main/java/com/trial/cloudGateway/util/JwtUtil.java
@@ -0,0 +1,57 @@
+package com.trial.cloudGateway.util;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public boolean validateToken(String token, String expectedEmail) {
+        try {
+            Claims claims = extractAllClaims(token);
+
+            if (isTokenExpired(claims)) {
+                return false;
+            }
+
+            String email = claims.getSubject();
+            return email.equals(expectedEmail);
+
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private boolean isTokenExpired(Claims claims) {
+        return claims.getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+}
